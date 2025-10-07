64576ff2d81f8d9669915b335120f689dba4c4d7
P-4L-0 <105886687+P-4L-0@users.noreply.github.com>
2025-10-07 17:28:28 -0600

    feat: new files to edit and desactivate "materias" fix(ver_grupo): fix the edit modal fix(editar_estudiante): correct the parameters of the querys

--- admin/editar_estudiante.php
+++ admin/editar_estudiante.php
@@ -12,17 +12,14 @@ $db = new Database();
 try {
     // Leer y sanitizar los datos enviados desde el formulario
     $id = intval($_POST['id']); // ID del estudiante a editar
-    $nie = trim($_POST['nie']); // Número de Identificación del Estudiante
     $nombre = trim($_POST['nombre_completo']); // Nombre completo del estudiante
     $grupo_id = intval($_POST['grupo_id']); // ID del grupo al que pertenece el estudiante
     
     // Preparar la consulta para actualizar los datos del estudiante
     $db->query("UPDATE estudiantes SET 
-               nie = :nie, 
                nombre_completo = :nombre,
                grupo_id = :grupo_id
                WHERE id = :id");
-    $db->bind(':nie', $nie); // Asociar el NIE del estudiante
     $db->bind(':nombre', $nombre); // Asociar el nombre del estudiante
     $db->bind(':grupo_id', $grupo_id); // Asociar el ID del grupo
     $db->bind(':id', $id); // Asociar el ID del estudiante
--- hash.php
+++ hash.php
@@ -1,4 +1,6 @@
 <?php
+// script para generar la contraseña del admin
+// run : consola > php hash.php
 $password = 'p4l0123';
 $hash = password_hash($password, PASSWORD_DEFAULT);
 echo $hash;
